615ab7482fda1941f43835b94b6dc44d67329842
Vinay Madenahally Prasad <vinaymp25@gmail.com>
2022-01-21 17:22:41 +0530

    Blank project

--- pages/portfolio/index.js
+++
@@ -1,9 +0,0 @@
-const Portfolio = () => {
-  return (
-    <div>
-      <h1>The Portfolio Page</h1>
-    </div>
-  );
-};
-
-export default Portfolio;
--- pages/portfolio/list.js
+++
@@ -1,9 +0,0 @@
-const PortfolioList = () => {
-  return (
-    <div>
-      <h1>The Portfolio List Page</h1>
-    </div>
-  );
-};
-
-export default PortfolioList;
